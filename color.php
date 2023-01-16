2c813f6696d57fd88bafc80efa209505fc3fec45
wawerusamuel <waweruzamuel@gmail.com>
2023-01-16 15:13:02 +0300

    first commit...

---
+++ color.php
@@ -0,0 +1,27 @@
+
+    <?php
+       
+     $color = "red"
+
+     switch($color){
+        case "red":
+        echo "the color is red"
+        break;
+
+        case "white":
+        echo "the color is white"
+        break;
+
+        case "green":
+        echo "the color is green"
+        break;
+
+        case "blue":
+        echo "the color is blue"
+        break;
+
+        default:
+        echo "wrong color"
+        break;
+     }
+    ?>
