af98f6295c74bb0d487ec7d9e14a2a44951f3370
wawerusamuel <waweruzamuel@gmail.com>
2023-01-17 15:52:58 +0300

    made some changes...

--- pages/signup.php
+++ pages/signup.php
@@ -10,6 +10,18 @@
     <link rel="stylesheet" type="text/css" href="../css/universal.css" />
   </head>
   <body>
+  <header class="header container border-bottom d-flex justify-content-between mt-4">
+      <div class="heading w-50 text-left align-items-center">
+        <h1>DATA COLLECTOR</h1>
+      </div>
+    <nav class="nav d-flex justify-content-end align-items-center">
+      <ul class="d-flex container-fluid list-unstyled font-weight-bold text-dark">
+        <li class="mr-4"><a href="./pages/signup.php">SIGNUP</a></li>
+        <li><a href="./pages/login.php">LOGIN</a></li>
+        <li class="ml-4"><a href="./pages/users.php">CONTACT</a></li>
+      </ul>
+    </nav>
+    </header>
     <fieldset class="container  bg-danger">
       <legend  class="text-center bg-danger w-50"><span class="text-white">DATA</span> COLLECTOR <sup>&reg;</sup></legend>
     <form action="" method="POST" class="form container">
@@ -26,7 +38,7 @@
         <input type="text" class="form-control" placeholder="Course" />
       </div>
       <div class="form-group">
-        <button class="btn btn-outline-success" type="submit">SIGN UP</button>
+        <button class="btn btn-success container" type="submit">SIGN UP</button>
       </div>
     </form>
     </fieldset>
