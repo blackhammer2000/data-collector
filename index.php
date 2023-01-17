463e21ff6f21c80c732c9c2e44ee7b93475423a2
wawerusamuel <waweruzamuel@gmail.com>
2023-01-17 15:01:32 +0300

    working on a data collector hero section...

--- index.php
+++ index.php
@@ -5,11 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
   </head>
   <body>
-    <header>
-
+    <header class="container d-flex border justify-content-between h-auto">
+      <div class="heading w-50 text-left align-items-center">
+        <h1>DATA COLLECTOR</h1>
+      </div>
+    <nav class="nav d-flex justify-content-end align-items-center">
+      <ul class="d-flex container-fluid list-unstyled">
+        <li class="mr-4"><a href="./signup.php">signup</a></li>
+        <li><a href="./login.php">login</a></li>
+        <li class="ml-4"><a href="./users.php">users</a></li>
+      </ul>
+    </nav>
     </header>
+
+    <section class="section hero-section h-75 border mt-2">
+      <h2><span class="text-danger">DATA</span> COLLECTOR.</h2>
+      <h4>Fast and precise data at your disposal.</h4>
+    </section>
    
     <!-- <form action="" method="POST">
       <input type="number" name="grade">
