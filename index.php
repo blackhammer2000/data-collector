dadcc0ef4ce7e30c133087d9adb98243be8b1859
wawerusamuel <waweruzamuel@gmail.com>
2023-01-17 15:28:15 +0300

    working on a data collector hero section...

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <h1>DATA COLLECTOR</h1>
       </div>
     <nav class="nav d-flex justify-content-end align-items-center">
-      <ul class="d-flex container-fluid list-unstyled font-weight-bold">
+      <ul class="d-flex container-fluid list-unstyled font-weight-bold text-dark">
         <li class="mr-4"><a href=".pages/signup.php">SIGNUP</a></li>
         <li><a href=".pages/login.php">LOGIN</a></li>
         <li class="ml-4"><a href=".pages/users.php">USERS</a></li>
@@ -22,18 +22,22 @@
     </nav>
     </header>
 
-    <section class="container section hero-section border mt-5 d-flex flex-column justify-content-start align-items-between text-center">
+    <section class="container section hero-section border mt-5 d-flex flex-column justify-content-between align-items-between text-center">
       <div class="hero container border d-flex justify-content-center align-items-center flex-column">
         <div class="hero-header container border w-100">
           <h2><span class="text-danger">DATA</span> COLLECTOR.</h2>
         </div>
-        <div class="hero-description">
+        <div class="hero-description mt-4">
           <h4>Fast and precise data at your disposal.</h4>
         </div>
       </div>
 
       <div class="container hero-quote">
-        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, dolore! Voluptatum, provident dolores. Obcaecati eum nostrum excepturi enim aliquam voluptate quaerat odio eveniet numquam delectus vitae beatae consequuntur dolore cum dolores atque neque, debitis sed ad recusandae earum repellat! Maiores fugit aliquid est? Eius dolorum dolores quos molestiae illum aspernatur sed architecto mollitia obcaecati expedita, explicabo repellendus, consequuntur commodi pariatur.</h6>
+      <span class="text-danger">
+      <h5>Welster Omwami~</h5>
+      <br>
+      </span>  
+        <blockquote class="font=weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, dolore! Voluptatum, provident dolores. Obcaecati eum nostrum excepturi enim aliquam voluptate quaerat odio eveniet numquam delectus vitae beatae consequuntur dolore cum dolores atque neque, debitis sed ad recusandae earum repellat! Maiores fugit aliquid est? Eius dolorum dolores quos molestiae illum aspernatur sed architecto mollitia obcaecati expedita, explicabo repellendus, consequuntur commodi pariatur.</blockquote>
       </div>
       
     </section>
