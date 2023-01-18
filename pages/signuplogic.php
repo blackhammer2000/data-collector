e3627f598ca23620d400126b91328c01a72f883c
wawerusamuel <waweruzamuel@gmail.com>
2023-01-18 20:23:34 +0300

    removed unwanted comments...

--- pages/signuplogic.php
+++ pages/signuplogic.php
@@ -20,68 +20,55 @@ if(isset($_POST["submit"])){
 
     if(empty($_POST["firstname"])){
         $errors["firstname"] =  "Firstname cannot be empty <br/>";
-        // echo "Firstname cannot be empty.";
     }
 
     if(!preg_match($textRegex, $firstName)){
         $errors["firstname"] = "Invalid First name";
-        // echo "Firstname is invalid.";
     }
 
     if(empty($_POST["lastname"])){
         $errors["lastname"] =  "Lastname cannot be empty <br/>";
-        // echo "Lastname cannot be empty.";
     }
 
     if(!preg_match($textRegex, $lastName)){
         $errors["lastname"] = "Invalid Lastname name";
-        // echo "Lastname is invalid.";
     }
 
     if(empty($_POST["email"])){
         $errors["email"] =  "Email cannot be empty <br/>";
-        // echo "Email cannot be empty.";
     }
 
     if(!preg_match($emailRegex, $email)){
         $errors["email"] = "Invalid Email";
-        // echo "Email is invalid.";
     }
 
     if(empty($_POST["password"])){
         $errors["password"] =  "Password cannot be empty <br/>";
-        // echo "Email cannot be empty.";
     }
 
     if(!preg_match($passwordRegex, $password)){
         $errors["password"] = "Password must be letters and numbers only";
-        // echo "Email is invalid.";
     }
 
     if(empty($_POST["confirmPassword"])){
         $errors["confirmPassword"] =  "Password cannot be empty <br/>";
-        // echo "Email cannot be empty.";
     }
 
     if(!preg_match($passwordRegex, $password)){
         $errors["confirmPassword"] = "Password must be letters and numbers only";
-        // echo "Email is invalid.";
     }
 
     if($password !== $confirmPassword){
         $errors["password"] = "Passwords do not match";
         $errors["confirmPassword"] = "Passwords do not match";
-        // echo "Email is invalid.";
     }
 
     if(empty($_POST["course"])){
         $errors["course"] =  "Course cannot be empty <br/>";
-        // echo "Course cannot be empty.";
     }
 
     if(!preg_match($textRegex, $course)){
         $errors["course"] = "Invalid Email";
-        // echo "Course is invalid.";
     }
 
 }
