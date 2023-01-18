8c81ef89873ddf93e09178d0782e58a522df05d5
wawerusamuel <waweruzamuel@gmail.com>
2023-01-18 15:41:09 +0300

    removed the class on the title and moved the php on the top of hthe page...

--- pages/signup.php
+++ pages/signup.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 
-<?php
-include("signuplogic.php")?>
+<?php require("signuplogic.php")?>
 
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title class="text-danger">DATA COLLECTOR</title>
+    <title>DATA COLLECTOR</title>
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="../css/signup.css" />
     <link rel="stylesheet" type="text/css" href="../css/universal.css" />
