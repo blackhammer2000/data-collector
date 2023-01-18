b186386cab555ebfc5581d55696b571bd7f4ad68
wawerusamuel <waweruzamuel@gmail.com>
2023-01-18 14:08:18 +0300

    working on the php logic for the form validation...

--- pages/signuplogic.php
+++ pages/signuplogic.php
@@ -2,7 +2,15 @@
 if(isset($_POST["submit"])){
     $firstName = $_POST["firstname"];
 
+    if(empty($_POST["firstname"])){
+        $errors["firstname"] =  "Firstname cannot be empty <br/>"
+    }
+
     
+
+    if(!preg_match(/^[a-zA-Z\s]+$/, $firstName)){
+        $errors["firstname"] = "Invalid First name"
+    }
 
 }
 
