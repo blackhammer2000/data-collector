162a81b9fafb88c889ad1fef27409a5889816193
wawerusamuel <waweruzamuel@gmail.com>
2023-01-18 13:56:33 +0300

    made someminor changes...

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <body>
     <header class="header container border-bottom d-flex justify-content-between mt-4">
       <div class="heading w-50 text-left align-items-center">
-        <h1>DATA COLLECTOR</h1>
+      <h1><span class="text-danger">DATA</span> COLLECTOR.</h1>
       </div>
     <nav class="nav d-flex justify-content-end align-items-center">
       <ul class="d-flex container-fluid list-unstyled font-weight-bold text-dark">
--- pages/signup.php
+++ pages/signup.php
@@ -12,20 +12,20 @@
   <body>
   <header class="header container border-bottom d-flex justify-content-between mt-4">
       <div class="heading w-50 text-left align-items-center">
-        <h1>DATA COLLECTOR</h1>
+      <h1><span class="text-danger">DATA</span> COLLECTOR<sup>&reg;</sup></h1>
       </div>
     <nav class="nav d-flex justify-content-end align-items-center">
       <ul class="d-flex container-fluid list-unstyled font-weight-bold text-dark">
-        <li class="mr-4"><a href="./pages/signup.php">SIGNUP</a></li>
-        <li><a href="./pages/login.php">LOGIN</a></li>
-        <li class="ml-4"><a href="./pages/users.php">CONTACT</a></li>
+        <li class="mr-4"><a href="./index.php">HOME</a></li>
+        <li><a href="./login.php">LOGIN</a></li>
+        <li class="ml-4"><a href="./contact.php">CONTACT</a></li>
       </ul>
     </nav>
     </header>
 
     <section class="border-bottom  pb-3 container section hero-section mt-5 d-flex flex-column justify-content-between align-items-between text-center">
     <fieldset class="container border-top">
-      <legend  class="text-center bg-danger w-50 mb-4"><span class="text-white">DATA</span> COLLECTOR <sup>&reg;</sup></legend>
+      <legend  class="text-center bg-danger w-50 mb-5"><span class="text-white">DATA</span> COLLECTOR <sup>&reg;</sup></legend>
     <form action="" method="POST" class="form container ">
       <div class="form-group">
         <input type="text" class="form-control" placeholder="Firstname" />
