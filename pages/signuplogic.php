60a1e64c184ea0896381be05d9494cc59d9c25ad
wawerusamuel <waweruzamuel@gmail.com>
2023-01-18 15:59:44 +0300

    saved the regex expressions as seperate variables...

--- pages/signuplogic.php
+++ pages/signuplogic.php
@@ -10,15 +10,15 @@ if(isset($_POST["submit"])){
     $email = $_POST["email"];
     $course = $_POST["course"];
 
-    // $textRegex = /^[a-zA-Z\s]+$/;
-    // $emailRegex = /^[a-zA-Z0-9]+$/;
+    $textRegex = '/^[a-zA-Z\s]+$/';
+    $emailRegex = '/^[a-zA-Z0-9]+$/';
 
     if(empty($_POST["firstname"])){
         $errors["firstname"] =  "Firstname cannot be empty <br/>";
         // echo "Firstname cannot be empty.";
     }
 
-    if(!preg_match('/^[a-zA-Z\s]+$/', $firstName)){
+    if(!preg_match($textRegex, $firstName)){
         $errors["firstname"] = "Invalid First name";
         // echo "Firstname is invalid.";
     }
@@ -28,7 +28,7 @@ if(isset($_POST["submit"])){
         // echo "Lastname cannot be empty.";
     }
 
-    if(!preg_match('/^[a-zA-Z\s]+$/', $lastName)){
+    if(!preg_match($textRegex, $lastName)){
         $errors["lastname"] = "Invalid Lastname name";
         // echo "Lastname is invalid.";
     }
@@ -38,7 +38,7 @@ if(isset($_POST["submit"])){
         // echo "Email cannot be empty.";
     }
 
-    if(!preg_match('/^[a-zA-Z0-9]+$/', $email)){
+    if(!preg_match($emailRegex, $email)){
         $errors["email"] = "Invalid Email";
         // echo "Email is invalid.";
     }
@@ -48,7 +48,7 @@ if(isset($_POST["submit"])){
         // echo "Course cannot be empty.";
     }
 
-    if(!preg_match('/^[a-zA-Z\s]+$/', $course)){
+    if(!preg_match($textRegex, $course)){
         $errors["course"] = "Invalid Email";
         // echo "Course is invalid.";
     }
