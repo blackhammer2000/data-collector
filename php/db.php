4913235bec2b137c0fc9bdff30b687f267725945
wawerusamuel <waweruzamuel@gmail.com>
2023-01-19 14:51:13 +0300

    working on the contact form logic...

--- php/db.php
+++ php/db.php
@@ -1,5 +1,20 @@
 <?php
 
+$host = "localhost";
+$database = "Students";
+$username = "root";
+$password = "";
 
+$dsn = "mysql:host=$host;dbname=$database";
+
+try {
+
+ $dbconnection = new PDO($dsn, $username, $password);
+ $dbconnection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ echo "Connection to the database is successfull";
+
+} catch (PDOException $err) {
+echo "Database connec tion failed".$err->getMessage();
+}
 
 ?>
