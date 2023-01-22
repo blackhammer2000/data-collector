dd20922904f1861388ca6d7ec7e805d42b557f53
wawerusamuel <waweruzamuel@gmail.com>
2023-01-22 10:57:34 +0300

    tried to create a mini program that counts the letters in a string...

--- pages/loginlogic.php
+++ pages/loginlogic.php
@@ -3,6 +3,20 @@
  $email  = $password = "";
  $errors = array("email" => "","password" => "");
 
+ $sentence = "helloworld";
+
+ $alphabets = array(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]);
+
+ $count = 0;
+
+ for ($j=0; $j < array_count_values(array([$sentence])); $j++) { 
+   for ($i=0; $i < array_count_values([$alphabets]); $i++) { 
+      if($i === $j) $count++;
+   }
+ }
+
+ echo $count;
+
  if(isset($_POST["submit"])){
 
     $email = $_POST["email"];
