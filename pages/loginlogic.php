6134d23f6c032e64418f14c8c523ab1279f6e3fe
wawerusamuel <waweruzamuel@gmail.com>
2023-01-26 16:48:12 +0300

    removed unwanted comments...

--- pages/loginlogic.php
+++ pages/loginlogic.php
@@ -3,20 +3,6 @@
  $email  = $password = "";
  $errors = array("email" => "","password" => "");
 
-//  $sentence = "helloworld";
-
-//  $alphabets = array(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]);
-
-//  $count = 0;
-
-//  for ($j=0; $j < array_count_values(array([$sentence])); $j++) { 
-//    for ($i=0; $i < array_count_values([$alphabets]); $i++) { 
-//       if($i === $j) $count++;
-//    }
-//  }
-
-//  echo $count;
-
  if(!isset($_POST["login"])) return;
 
     $email = $_POST["email"];
